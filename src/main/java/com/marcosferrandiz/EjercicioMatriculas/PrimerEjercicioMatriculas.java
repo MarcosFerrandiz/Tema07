ec739b6a72ea7c5f5e5eed023c5e15cfc5e767f6
MarcosFerrandiz <marcos149ft@gmail.com>
2025-03-25 13:14:43 +0100

    Javadoc, sigo sin saber si esta bien la verdad

--- src/main/java/com/marcosferrandiz/EjercicioMatriculas/PrimerEjercicioMatriculas.java
+++ src/main/java/com/marcosferrandiz/EjercicioMatriculas/PrimerEjercicioMatriculas.java
@@ -11,6 +11,11 @@ public class PrimerEjercicioMatriculas {
         System.out.println(matricula);
     }
 
+    /**
+     * Solicita la matricula al usuario, en el caso de que lo haya escrito mal saldrá una excepcion
+     * @return Devuelve la matricula, de forma valida
+     * @throws MatriculaInvalidaException Excepción en el caso de que ponga mal la matricula
+     */
     public static String solicitarMatricula() throws MatriculaInvalidaException{
         String matricula = leerString("Introduzca la matricula con un espacio entre los numeros y las letras",0,8);
         if (validarMatricula(matricula)){
@@ -20,6 +25,13 @@ public class PrimerEjercicioMatriculas {
     }
 
 
+    /**
+     * Metodo para leer un String con un máximo y con un minimo, poniendo el mensaje que queremos sacar por pantalla
+     * @param mensaje El mensaje que queremos sacar por pantalla
+     * @param minLength El maximo del String
+     * @param maxLength El minimo del String
+     * @return Devuelve el String que haya introducido el usuario
+     */
     public static String leerString(String mensaje, int minLength, int maxLength) {
         boolean valido;
         String valor;
@@ -35,6 +47,11 @@ public class PrimerEjercicioMatriculas {
         return valor;
     }
 
+    /**
+     * Metodo que valida que una matricula se ha introducido de forma correcta en formato [NNNN LLL] donde las N son numeros y las L son letras
+     * @param matricula La matricula que queremos validar
+     * @return Devuelve true si cumple todos los requisitos y false si no los cumple
+     */
     public static boolean validarMatricula(String matricula) {
         int i;
         if (matricula.length() != 8) {
